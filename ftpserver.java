b369ba42967ee7e2750b5cc0dffb7a14fd76cd3a
DavidBPowers <55028524+DavidBPowers@users.noreply.github.com>
2021-04-17 19:47:05 -0400

    Game Logic
    This is the first iteration of the game logic

--- ftpserver.java
+++ ftpserver.java
@@ -13,6 +13,9 @@ public class ftpserver extends Thread {
         private Socket connectionSocket;
         int port;
         int count = 1;
+        int turn = 1;
+        char disc = 'R';
+        char[][] board = new char[6][7];
 
         public ftpserver(Socket connectionSocket) {
             this.connectionSocket = connectionSocket;
@@ -32,7 +35,6 @@ public class ftpserver extends Thread {
             }
 
         }
-
 
         private void processRequest() throws Exception {
             String fromClient;
@@ -145,8 +147,107 @@ public class ftpserver extends Thread {
 			break;
 			
 		}
-            }
         }
     }
+    private initBoard(){
+            for (int row = 0; row < board.length; row++){
+                for (int col = 0; col < board[0].length; col++){
+                    board[row][col] = ' ';
+                }
+            }
+    }
+    private void placeDiscAt(col){
+            if (!isValidMove(col)){
+                return;
+            }
+            int row = findRow(col);
+
+            board[row][col] = disc;
+    }
+    private void prepareNextTurn(){
+            if (turn == 1){
+                turn = 2;
+            }
+            else {
+                turn = 1;
+            }
+            if (disc == 'R'){
+                disc = 'Y';
+            }
+            else {
+                disc = 'R';
+            }
+    }
+    private boolean isBoardFull(){
+            for (int row = 0; row < board.length; row++)
+            {
+                for (int col = 0; col < board[0].length; col++)
+                {
+                    if (board[row][col] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+    }
+    //Do not change disc before checking winner
+    private char checkWinner(col){
+            int rowMove = 0;
+            int colMove = 0;
+            int row = findRow(col);
+
+            for (int i = -1; i < 2; i++){
+                for (int j = -1; j < 2; j++){
+                    if (row + i < 0 || row + i >= board.length || col + j < 0 || col + j >= board[0].length || (i == 0 && j == 0)){
+                        continue;
+                    }
+                    if (board[row + i][col + j] == disc)
+                    {
+                        rowMove = row + i;
+                        colMove = col + j;
+
+                        if (rowMove + i < 0 || rowMove + i >= board.length || colMove + j < 0 || colMove + j >= board[0].length)
+                        {
+                            continue;
+                        }
+
+                        if (board[rowMove + i][colMove + j] == disc)
+                        {
+                            rowMove += i;
+                            colMove += j;
+
+                            if (rowMove + i < 0 || rowMove + i >= board.length || colMove + j < 0 || colMove + j >= board[0].length)
+                            {
+                                continue;
+                            }
+                            
+                            if (board[rowMove + i][colMove + j] == disc)
+                            {
+                                return disc;
+                            }
+                        }
+                    }
+                }
+            }
+            return ' ';
+
+
+    }
+    private int findRow(col){
+            for (int row = 0; row < board.length; row++){
+                if (board[row][col] != ' '){
+                    return row - 1;
+                }
+            }
+    }
+    private boolean isValidMove(col){
+            if (findRow(col) < 0){
+                return false;
+            }
+            return true;
+    }
+
+}
 	
 
